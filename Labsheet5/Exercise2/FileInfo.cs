8b8631f436d1dbbbe25fc8425a87bf6985bced53
Janessa <s00190968@mail.itsligo.ie>
2019-03-20 14:45:06 +0000

    Exercise2 finished

---
+++ Labsheet5/Exercise2/FileInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    public class FileInfo
+    {
+        public string Name { get; set; }
+        public long Length { get; set; }
+        public DateTime CreationTime { get; set; }
+    }
+}
---
+++ Labsheet5/Exercise2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var files = new DirectoryInfo("C:\\Windows").GetFiles();
+
+            var query = files.Where(f => f.Length < 1000).OrderBy(f => f.Length).ThenBy(f => f.Name)
+                .Select(f => new FileInfo
+                {
+                    Name = f.Name,
+                    Length = files.Length,
+                    CreationTime = f.CreationTime
+                });
+
+            Console.WriteLine("{0,0}{1,20}{2,40}", "Name", "Length", "Creation date");
+
+            foreach(var item in query)
+            {
+                Console.WriteLine("{0,0}{1,20}{2,40}", item.Name,item.Length,item.CreationTime.ToShortDateString());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
