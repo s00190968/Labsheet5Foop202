f5c4941c111c043647edde43990f6f2de3dd91a0
Janessa <s00190968@mail.itsligo.ie>
2019-03-20 14:56:18 +0000

    Exercise 4 finished.

---
+++ Labsheet5/Exercise4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] nums = { 3, 32, 34, 435, 32, 3243, 5464, 23, 221, 35, 56, 57, 7, 643, 52, 2, 35, 67 };
+
+            var query = nums.Select(n => DoubleIt(n));
+
+            Console.WriteLine("Before the foreach loop");
+
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+
+        private static int DoubleIt(int value)
+        {
+            Console.WriteLine("Doubling the value");
+            return value * 2;
+        }
+    }
+}
