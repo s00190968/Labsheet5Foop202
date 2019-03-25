7065c7aed200f59b77a257cac5b21d1472bc101a
Janessa Aulén <S00190968@mail.itsligo.ie>
2019-03-25 18:29:38 +0000

    Exercise 9 finished

---
+++ Labsheet5/Exercise9/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format($"{Name}, {City}");
+        }
+    }
+}
---
+++ Labsheet5/Exercise9/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var query = GetCustomers().Select(n => n).Where(n => n.City.Contains("Dublin"));
+
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+
+        }
+        private static List<Customer> GetCustomers()
+        {
+            List<Customer> customers = new List<Customer>();
+            customers.Add(new Customer { Name = "Tom", City = "Dublin" });
+            customers.Add(new Customer { Name = "Sally", City = "Galway" });
+            customers.Add(new Customer { Name = "George", City = "Cork" });
+            customers.Add(new Customer { Name = "Molly", City = "Dublin" });
+            customers.Add(new Customer { Name = "Joe", City = "Galway" });
+            customers.Add(new Customer { Name = "Coldsteel The Hedgeheg", City = "Dublin" });
+
+            return customers;
+        }
+    }
+}
